4726e25dc6d1b5aa8bb7eb37afe23d9dea3b2cff
ellie529 <40528667+ellie529@users.noreply.github.com>
2018-07-04 16:21:36 -0400

    Update deploy.sh
    edited IP

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-cd /c/code/CV; git pull; scp -i ~/.ssh/ellie_resume.pem * ec2-user@34.207.84.16:/usr/share/ellie_resume; ssh -i ~/.ssh/ellie_resume.pem root@34.207.84.16 'systemctl restart nginx';
+cd /c/code/CV; git pull; scp -i ~/.ssh/ellie_resume.pem * ec2-user@35.171.23.37:/usr/share/ellie_resume; ssh -i ~/.ssh/ellie_resume.pem root@35.171.23.37 'systemctl restart nginx';
